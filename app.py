2a68d00910ce3a9360ca175d2b28bb87d4a48d28
lifeparticle <mahbubzaman17@gmail.com>
2022-12-24 13:53:11 +1100

    handle better

--- app.py
+++ app.py
@@ -3,7 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = ''
-    # if 'name' in request.args:
     name = request.args.get('name', '')
     return f'Hello, {name}!' if name != "" else "Hello, stranger!"
